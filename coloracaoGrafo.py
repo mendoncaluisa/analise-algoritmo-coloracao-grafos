d4c7229ce8115a5ceffa4697800ea99ce8e1773c
Maria Luísa Mendonça Oliveira <0049545@academico.ifmg.edu.br>
2024-08-31 10:17:40 -0300

    codigo de conduta

--- coloracaoGrafo.py
+++ coloracaoGrafo.py
@@ -1,3 +1,18 @@
+# Nome(s) Discente(s): Maria Luísa Mendonça Oliveira, Poliana Cristina de Sousa
+# Matrícula(s): 0049545, 
+# Data: agosto de 2024
+
+
+# Declaro que sou o único autor e responsável por este programa. Todas as partes do programa, exceto as que 
+# foram fornecidas pelo professor foram desenvolvidas por mim. Declaro também que
+# sou responsável por todas  as eventuais cópias deste programa e que não distribui nem facilitei a #distribuição de cópias.
+
+######/Leia e depois apague  os comentários abaixo #############
+
+# Escreva um comentário no início do arquivo fonte para indicar O QUE o código faz
+# Coloque comentários no seu código de modo a explicar o que está sendo feito
+# Se utilizar ou basear seu código em alguma fonte externa, explicite este fato.
+
 def coloracao_gulosa(grafo):
     # Inicializa o dicionário para armazenar a cor de cada nó
     coloracao = {}
---
+++ main.py
@@ -0,0 +1,8 @@
+# Nome(s) Discente(s): Maria Luísa Mendonça Oliveira, Poliana Cristina de Sousa
+# Matrícula(s): 0049545, 
+# Data: agosto de 2024
+
+
+# Declaro que sou o único autor e responsável por este programa. Todas as partes do programa, exceto as que 
+# foram fornecidas pelo professor foram desenvolvidas por mim. Declaro também que
+# sou responsável por todas  as eventuais cópias deste programa e que não distribui nem facilitei a #distribuição de cópias.
